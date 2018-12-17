58770ae89be2e99b5c561b23c8e72e09e4896be6
kazshige <kazuki.shigemitsu@gmail.com>
2018-12-17 14:18:48 -0800

    add multiple values in an array

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ $(document).ready(function(){
   console.log('jQuery loaded');
 
   // write to local storage from input when button save clicked
-  $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
+  $('.btn-submit').on('click', function() {
     var myItemInStorage = localStorage.getItem('inputFieldValue');
+
+    if(myItemInStorage) {
+      myItemInStorage = JSON.parse(myItemInStorage);
+    } else {
+      myItemInStorage = [];
+    }
+    
+    var fieldValue = $('.text-entry').val();
+
+    myItemInStorage.push(fieldValue);
+
+    localStorage.setItem('inputFieldValue', JSON.stringify(myItemInStorage));
+    
     console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
     $('.list-display-field').text(myItemInStorage); // ??
-
   });
-
+  
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
     localStorage.removeItem('inputFieldValue');
